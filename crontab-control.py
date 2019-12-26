1f770f34a4bf676c2230ace6eb43e16fc3b57939
rabbit721 <001207jiaojie@gmail.com>
2019-12-26 10:33:25 +0800

    first attempt at crontab

---
+++ crontab-control.py
@@ -0,0 +1,14 @@
+from crontab import CronTab
+
+my_cron = CronTab(user=True)
+
+path = '/users/k.j./Documents/GitHub/zhihu-spider'
+job = my_cron.new(command='/Library/Frameworks/Python.framework/Versions/3.7'
+                          '/bin/python3 {}/spider.py >> ~/latest_update.log'
+                          .format(path, path))
+print('started new job :-)')
+job.setall('0 * * * *')
+job.set_comment("spider crontab job")
+
+job.enable()
+my_cron.write()
