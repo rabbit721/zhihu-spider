7e1c270665fecd2b883228c656de3b831707ad46
rabbit721 <001207jiaojie@gmail.com>
2019-12-27 16:22:24 +0800

    added parsing for input arguments

--- crontab-control.py
+++ crontab-control.py
@@ -1,13 +1,24 @@
 from crontab import CronTab
+import sys
 
 my_cron = CronTab(user=True)
 
-path = '/users/k.j./Documents/GitHub/zhihu-spider'
-job = my_cron.new(command='/Library/Frameworks/Python.framework/Versions/3.7'
-                          '/bin/python3 {}/spider.py >> ~/latest_update.log'
-                          .format(path, path))
+if len(sys.argv) < 4:
+    print("Expecting 3 input args, got {}".format(str(len(sys.argv) - 1)))
+    exit(1)
+
+# excel_path = '/code/docker_result.xls
+# mongdb_name = 'mongodb'
+python_path = sys.argv[1]
+path = sys.argv[2]
+mongdb_name = sys.argv[3]
+
+job = my_cron.new(command='{}/python {}/spider.py {} {} >> '
+                          '{}/latest_update.log'.format(python_path, path, path,
+                                                        mongdb_name, path))
+
 print('started new job :-)')
-job.setall('0 * * * *')
+job.setall('0 0 * * *')
 job.set_comment("spider crontab job")
 
 job.enable()
